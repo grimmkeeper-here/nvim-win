0d2a5012a90dba0082298d0871023bebd4e8e348
ncvinh123@gmail.com <ncvinh123@gmail.com>
2024-09-10 13:42:40 +0700

    Modi: adapt with new lsp version
    - Correct ts language server to adapt with new LSP version

--- lua/plugin/lsp/lang/ts.lua
+++ lua/plugin/lsp/lang/ts.lua
@@ -1,5 +1,5 @@
 require "plugin.lsp.utils"
-require('lspconfig')['tsserver'].setup{
+require('lspconfig')['ts_ls'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
     capabilities = capabilities
