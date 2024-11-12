703fcc236e6b6fda190df9bc10278372b8deb2d7
ncvinh123@gmail.com <ncvinh123@gmail.com>
2024-11-12 14:07:34 +0700

    Fix: fix deprecated cmd lsp

--- lua/plugin/lsp/lang/proto.lua
+++ lua/plugin/lsp/lang/proto.lua
@@ -1,5 +1,5 @@
 require "plugin.lsp.utils"
-require('lspconfig')['bufls'].setup{
+require('lspconfig')['buf_ls'].setup {
     on_attach = on_attach,
     flags = lsp_flags,
     capabilities = capabilities
